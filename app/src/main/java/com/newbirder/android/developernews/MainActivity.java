ba816f5280a3a6b6563addca739e4a9c893642dc
Newbirder <feixiangdream@163.com>
2015-12-06 22:28:46 +0800

    Revert "删除部分没用代码"
    This reverts commit c6d19c73faeaea77ef324894192f747a6de0e9aa.

--- app/src/main/java/com/newbirder/android/developernews/MainActivity.java
+++ app/src/main/java/com/newbirder/android/developernews/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 refresher.setRefreshing(false);
             }
         });
+//        fetchLatestNews();
     }
 
     private void fetchLatestNews() {
